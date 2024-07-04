723eadb8f4966b3ef92b6be930edefc4001cdb93
LuccaACV <lucca.valente@gmail.com>
2024-07-04 19:16:55 -0300

    feat: add exerc3

---
+++ Exercicios/Exerc3.js
@@ -0,0 +1,22 @@
+
+/*
+Faça um algoritmo em que você recebe 3 notas de um aluno e caso a
+média aritmética dessas notas for maior ou igual que 6 imprima
+“Aprovado”, caso contrário “Reprovado”.
+*/
+
+var media = 0.0
+for(var cont = 1; cont<=3; cont++){
+    var temp = prompt("Insira o valor da "+ cont +"° prova: ")
+    media = parseFloat(temp) + media
+}
+
+media=media/3
+console.log(media)
+
+if(media>=6){
+    console.log("APROVADO")
+}
+else{
+    console.log("REPROVADO")
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 <h1>Lista Processo Seletivo Injunior<h1>
+<h2>Respostas para o desafio JavaScript<h2>
+<p>Necessita trocar o endereço do arquivo .JS no html<p>
