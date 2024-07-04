599ad27c622e6520ff72703098fcb34a3fda8cc1
LuccaACV <lucca.valente@gmail.com>
2024-07-04 20:04:01 -0300

    feat: add codigo exercicio5

--- Exercicios/Exerc5.js
+++ Exercicios/Exerc5.js
@@ -5,3 +5,20 @@ Teste 5 números inteiros aleatórios. Os testes:
 ● Caso o valor seja divisível por 3 e 5, ao mesmo tempo, imprima “fizzbuzz”.
 ● Caso contrário imprima nada.
 */
+
+for(var cont=1; cont<=5; cont++){
+    var tempNum = prompt("Entre com o "+cont+"°: ")
+    var tempNum = parseInt(tempNum)
+
+    if((tempNum%3==0)&&(tempNum%5==0)){
+        console.log("fizzbuzz")
+    }
+    else{
+        if(tempNum%3==0){
+            console.log("fizz")
+        }
+        if(tempNum%5==0){
+            console.log("buzz")
+        }
+    }
+}
