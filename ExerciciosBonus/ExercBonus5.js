a101c1f08d31aedcb8ba3a76a36239a48ed18fb1
LuccaACV <lucca.valente@gmail.com>
2024-07-05 18:28:40 -0300

    feat: add codigo do ExercBonus5

--- ExerciciosBonus/ExercBonus5.js
+++ ExerciciosBonus/ExercBonus5.js
@@ -1,16 +1,3 @@
-/* 
-Baseado no Array de Livros por Categoria abaixo, faça os seguintes
-desafios:
-
-● Contar o número de categorias e o número de livros em cada categoria
-
-● Contar o número de autores
-
-● Mostrar livros do autor Augusto Cury
-
-● Transformar a função acima em uma função que irá receber o 
-nome do autor e devolver os livros desse autor.
-*/
 
 const booksByCategory = [
     {
@@ -48,3 +35,71 @@ const booksByCategory = [
             ]
     }
 ];
+
+//Função da parte 4
+function imprimeLivro(nome){
+    console.log("Os livros do "+nome+" são: ")
+    for(let LivrosCat of booksByCategory){
+        for(let Livros of LivrosCat.books){
+            if(Livros.author==nome){
+                console.log(Livros.title)
+            }
+        }
+     }
+}
+
+var dec = prompt("Qual questão quer visualizar? 1,2,3 ou 4 ?") 
+dec = parseInt(dec)
+
+switch(dec){
+
+    case 1:
+        for(let LivrosCat of booksByCategory){
+           console.log("Na Categoria: "+LivrosCat.category+"\nExistem ("+LivrosCat.books.length+") livros!!")
+        }
+        break;
+
+    case 2:
+        var autores = []
+        var cont
+        for(let LivrosCat of booksByCategory){
+            for(let Livros of LivrosCat.books){
+                cont = 0
+                for(let autorTemp of autores){
+                    if(Livros.author==autorTemp){
+                        cont++
+                    }
+                }
+                if(cont==0){
+                    autores.push(Livros.author)
+                }
+            }
+         }
+         console.log("A quantidade de autores unicos nessa coleção de livros são "+autores.length)
+        break;
+
+    case 3:
+        console.log("Os livros do Augusto Cury são: ")
+        for(let LivrosCat of booksByCategory){
+            for(let Livros of LivrosCat.books){
+                if(Livros.author=="Augusto Cury"){
+                    console.log(Livros.title)
+                }
+            }
+         }
+        break;
+
+    case 4:
+        var nomeA = ["Augusto Cury","Stephen R. Covey","Robert T. Kiyosaki e Sharon L. Lechter","George S. Clason","T. Harv Eker"]
+        var num = prompt("Qual nome do autor que deseja buscar?\n"+nomeA[0]+"- 0\n"+nomeA[1]+" - 1\n"+nomeA[2]+" - 2\n"+nomeA[3]+" - 3\n"+nomeA[4]+" - 4")
+        while(num>4||num<0){
+            alert("Valor invalido!!")
+            var num = prompt("Qual nome do autor que deseja buscar?\n"+nomeA[0]+"- 0\n"+nomeA[1]+" - 1\n"+nomeA[2]+" - 2\n"+nomeA[3]+" - 3\n"+nomeA[4]+" - 4")
+        }
+        imprimeLivro(nomeA[num])
+        break;
+
+    default:
+        alert("Valor invalido, escolha 1,2,3 ou 4!!")
+        break;
+}
