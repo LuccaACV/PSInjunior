ac2cd74c0537171fe76a63841c93ae9c70031fa5
LuccaACV <lucca.valente@gmail.com>
2024-07-05 17:26:17 -0300

    feat: add switch no Exerc4 e completando o codigo

--- Exercicios/Exerc4.js
+++ Exercicios/Exerc4.js
@@ -16,57 +16,69 @@ Por exemplo, o array deverá ficar assim: ["Achilles (Warrior)", "Agni (Mage)", 
 //Importando o objeto gods para o Exercicio 4 
 import { exportaDeuses } from './Exercicio4/arquivo_exercicio_4.js';
 const deuses = exportaDeuses()
-/*
-//Q1
-for(let deus of deuses){
-    console.log(deus.name+": "+deus.features.length)
-}
 
-//Q2
-let num = 0
-for(let Deus of deuses){
-    for(let cont in Deus.roles){
-        if(Deus.roles[cont]=="Mid"){
-            let json = JSON.stringify(Deus)
-            console.log(num+" "+json)
-            num++
-        }
-    }
-}
+var dec = prompt("Qual questão quer visualizar? 1,2,3 ou 4 ?") 
+dec = parseInt(dec)
 
-//Q3
-var arrayPanteao = []
-for(let Deus of deuses){
-    let cont = 0;
-    for(let CompString of arrayPanteao){
-        if(Deus.pantheon==CompString){
-            cont++;
-        }
-    }
-    if(cont==0){
-        arrayPanteao.push(Deus.pantheon)
-    }
-}
-let num = 0
-arrayPanteao.sort()
-console.log(arrayPanteao)
-for(let compPanteao of arrayPanteao){
-    for(let Deus of deuses){
-        if(Deus.pantheon==compPanteao){
-            let json = JSON.stringify(Deus)
-            console.log(num+" "+json)
-            num++
-        }
-    }
-}
-*/
+switch (dec) {
 
-//Q4
-var arrayNomeFunc = []
-for(let Deus of deuses){
-    let stringNome = Deus.name+" ("+Deus.class+")"
-    arrayNomeFunc.push(stringNome)
-}
-for(let nomeDeus of arrayNomeFunc){
-    console.log(nomeDeus)
+    case 1:
+        for(let deus of deuses){
+            console.log(deus.name+": "+deus.features.length)
+        }
+        break;
+
+    case 2:
+        let num = 0
+        for(let Deus of deuses){
+            for(let cont in Deus.roles){
+                if(Deus.roles[cont]=="Mid"){
+                    let json = JSON.stringify(Deus)
+                    console.log(num+" "+json)
+                    num++
+                }
+            }
+        }
+        break;
+
+    case 3:
+            var arrayPanteao = []
+            for(let Deus of deuses){
+                let cont = 0;
+                for(let CompString of arrayPanteao){
+                    if(Deus.pantheon==CompString){
+                        cont++;
+                    }
+                }
+                if(cont==0){
+                    arrayPanteao.push(Deus.pantheon)
+                }
+            }
+            let numTemp = 0
+            arrayPanteao.sort()
+            for(let compPanteao of arrayPanteao){
+                for(let Deus of deuses){
+                    if(Deus.pantheon==compPanteao){
+                        let json = JSON.stringify(Deus)
+                        console.log(numTemp+" "+json)
+                        numTemp++
+                    }
+                }
+            }
+        break;
+
+    case 4:
+        var arrayNomeFunc = []
+        for(let Deus of deuses){
+            let stringNome = Deus.name+" ("+Deus.class+")"
+            arrayNomeFunc.push(stringNome)
+        }
+        for(let nomeDeus of arrayNomeFunc){
+            console.log(nomeDeus)
+        }
+        break;
+
+    default:
+        alert("Valor invalido, escolha 1,2,3 ou 4!!")
+        break;
 }
