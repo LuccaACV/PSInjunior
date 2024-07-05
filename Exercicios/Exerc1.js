bae8557a98cdfbdb9984139561502b12a10c193a
LuccaACV <lucca.valente@gmail.com>
2024-07-05 19:33:28 -0300

    feat: adicionando funcao de criarMatriz em Exerc1

--- Exercicios/Exerc1.js
+++ Exercicios/Exerc1.js
@@ -6,35 +6,57 @@ igual ao número de linhas da segunda matriz. (2x2)
 
 a. Caso teste 1 : [ [ [2],[-1] ], [ [2],[0] ] ] e [ [2,3],[-2,1] ] 
 multiplicadas dão [ [6,5], [4,6] ]
-
+    Seguencia de entrada:
+    2 2 2 -1 2 0    2 2 2 3 -2 1
 b. Caso teste 2 : [ [4,0], [-1,-1] ] e [ [-1,3], [2,7] ] 
 multiplicadas dão [ [-4,12], [-1,-10]] 
+    Seguencia de entrada:
+    2 2 4 0 -1 -1   2 2 -1 3 2 7
 */
 
-var matrizA = [ [4,0], [-1,-1] ]
-var linhaA = matrizA.length
-var colunaA = matrizA[0].length
-
-var matrizB = [ [-1,3], [2,7] ]
-var linhaB = matrizB.length
-var colunaB = matrizB[0].length 
-
-var matrizC = [] //qtdLinha da primeira com a qtdLinha da segunda
-
-for(let i = 0; i<linhaA;i++){
-    matrizC.push(Array(colunaB))
-}
-for(let i = 0; i<linhaA;i++){
-    for(let j = 0; j<colunaB;j++){
-        var temp = 0
-        for(let k = 0; k<colunaA;k++){
-            temp += (matrizA[i][k])*(matrizB[k][j])
-            console.log(temp)
+function criandoMatriz(linha,coluna){
+    let array = []
+    for(let l = 0; l<linha;l++){
+        array.push([]);
+        for(let c = 0; c<coluna;c++){
+            let num = prompt("Adicione um valor na linha "+(l+1)+" da "+(c+1)+" coluna:")
+            array[l].push(num)
         }
-        matrizC[i][j] = temp
     }
+    return array
 }
 
-for(let i = 0; i<matrizC.length; i++){
-    console.log(matrizC[i])
+var linhaA = prompt("Qual a quantidade de linhas da 1° Matriz?")
+var colunaA = prompt("Qual a quantidade de colunas da 1° Matriz?")
+var linhaA = parseInt(linhaA)
+var colunaA = parseInt(colunaA)
+
+var linhaB = prompt("Qual a quantidade de linhas da 2° Matriz?")
+var colunaB = prompt("Qual a quantidade de colunas da 2° Matriz?")
+var linhaB = parseInt(linhaB)
+var colunaB = parseInt(colunaB)
+
+if(colunaA==linhaB){
+    var matrizA = criandoMatriz(linhaA,colunaA)
+    var matrizB = criandoMatriz(linhaB,colunaB)
+    var matrizC = [] 
+    for(let i = 0; i<linhaA;i++){
+        matrizC.push(Array(colunaB))
+    }
+    for(let i = 0; i<linhaA;i++){
+        for(let j = 0; j<colunaB;j++){
+            var temp = 0
+            for(let k = 0; k<colunaA;k++){
+                temp += (matrizA[i][k])*(matrizB[k][j])
+                console.log(temp)
+            }
+            matrizC[i][j] = temp
+        }
+    }
+
+    for(let i = 0; i<matrizC.length; i++){
+        console.log(matrizC[i])
+    }
+}else{
+    console.log("O numero de colunas da 1° Matriz é diferente do numero de linhas da 2° Matriz!!!")
 }
